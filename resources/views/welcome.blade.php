3eff6e7193f880a806635ed38df3aade72b5dd79
Mattia Iacona <mattiaiacona87@icloud.com>
2024-10-12 01:53:59 +0200

    aggiunta img profilo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,12 @@
 <x-layout>
-    <div class="flex bg-gray-800 h-64 font-bold items-center justify-center">
-      <h1 class="text-6xl text-gray-300">&lt;I&period;M&period;Dev&gt;</h1>
-    </div>
+  <header class="flex bg-gray-800 h-64 items-center justify-center">
+      <h1 class="text-6xl text-gray-300 font-black">&lt;I&period;M&period;DEV&gt;</h1>
+  </header>
+  <section class="flex items-center md:h-auto m-12">
+      <div class="w-full md:w-1/2 flex justify-center">
+        <div class="relative group">
+          <img src="images/profilo.png" alt="" class="w-64 rounded-2xl transition-transform duration-300 ease-in-out transform group-hover:scale-110">
+        </div>  
+      </div>
+  </section>
 </x-layout>
