1ff9f8c55d3698b39afc6a1b7585bfb94f38c965
Mattia Iacona <mattiaiacona87@icloud.com>
2024-10-12 01:08:41 +0200

    aggiunto font e bg

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,9 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title ?? config('app.name') }}</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link href="https://fonts.googleapis.com/css2?family=Sequel:wght@400;700&display=swap" rel="stylesheet">
+    @vite('resources/css/app.css')
 </head>
-<body>
+<body class="bg-gray-900 font-sequel">
     {{ $slot }}
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,5 @@
 <x-layout>
-    ciao come stai
+    <div class="flex bg-gray-800 h-64 font-bold items-center justify-center">
+      <h1 class="text-6xl text-gray-300">&lt;I&period;M&period;Dev&gt;</h1>
+    </div>
 </x-layout>
