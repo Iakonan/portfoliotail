d312ea18b7ee55e0f74ac56a7c62cb354b58a72f
Mattia Iacona <mattiaiacona87@icloud.com>
2024-10-12 03:20:51 +0200

    cambio immagine profilo con disegno e aggiunta bio ma ancora da sistemare

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,16 @@
   <section class="flex items-center md:h-auto m-12">
       <div class="w-full md:w-1/2 flex justify-center">
         <div class="relative group">
-          <img src="images/profilo.png" alt="" class="w-64 rounded-2xl transition-transform duration-300 ease-in-out transform group-hover:scale-110">
-        </div>  
+          <img src="images/profile.png" alt="" style="width: 400px" class="rounded-2xl transition-transform duration-300 ease-in-out transform group-hover:scale-110">
+        </div>
+      </div>
+      <div class="w-full md:w-1/2 flex justify-center">
+        <div>
+          <h2 class="text-gray-300 text-2xl font-bold">Mattia Iacona, classe 1987. Originario di Ravenna, sono sempre stato appassionato di tecnologia, ma da ragazzo ho seguito un’altra mia grande passione: la cucina. Mi sono diplomato alla scuola alberghiera di Cervia e ho lavorato per anni in ristoranti e alberghi della riviera romagnola, fino ai 30 anni. A quel punto, ho deciso di dare una svolta alla mia vita e di dedicarmi a qualcosa che valorizzasse meglio il mio tempo.
+            Attualmente, lavoro come impiegato data entry e addetto alla sala posta, ma da novembre 2023 ho iniziato un nuovo percorso nella programmazione web e mobile. Dopo aver seguito un corso con Aulab e ottenuto la certificazione, ho continuato a studiare come autodidatta. Al momento, utilizzo Laravel, PHP, Bootstrap e JavaScript, con l’obiettivo di espandere le mie competenze a Flutter, TypeScript, Angular e Next.js.            
+            La mia curiosità e determinazione mi spingono a superare ogni ostacolo con il mantra: “Non ho la minima idea di quello che sto facendo, e per questo sono invincibile.” Oltre alla programmazione, le mie passioni spaziano dalla musica all’antropologia.
+          </h2>
+        </div>
       </div>
   </section>
 </x-layout>
