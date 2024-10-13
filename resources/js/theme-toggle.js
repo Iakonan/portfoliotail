1ba8a3e66f20d7b1c31a6820ca9c67e1992cc1b5
Mattia Iacona <mattiaiacona87@icloud.com>
2024-10-13 14:01:31 +0200

    sistemato footer, modalità chiaro/scuro e layout

--- resources/js/theme-toggle.js
+++ resources/js/theme-toggle.js
@@ -6,17 +6,17 @@ themeToggleButton.addEventListener('click', () => {
     // Toggling between dark and light mode
     if (body.classList.contains('bg-stone-950')) {
         body.classList.remove('bg-stone-950', 'text-gray-300');
-        body.classList.add('bg-gray-100', 'text-gray-900');
+        body.classList.add('bg-gray-100', 'text-stone-950');
 
         // Cambia il footer
         footer.classList.remove('bg-stone-950', 'text-gray-300');
-        footer.classList.add('bg-gray-100', 'text-gray-900');
+        footer.classList.add('bg-gray-100', 'text-stone-950');
     } else {
-        body.classList.remove('bg-gray-100', 'text-gray-900');
-        body.classList.add('bg-stone-950', 'text-lime-300');
+        body.classList.remove('bg-gray-100', 'text-stone-950');
+        body.classList.add('bg-stone-950', 'text-gray-300');
 
         // Cambia il footer
-        footer.classList.remove('bg-gray-100', 'text-gray-900');
+        footer.classList.remove('bg-gray-100', 'text-stone-950');
         footer.classList.add('bg-stone-950', 'text-gray-300');
     }
 });
--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,20 +1,20 @@
 <footer id="footer" class="bg-stone-950 text-gray-300 py-4 fixed bottom-0 left-0 w-full">
     <div class="flex flex-col md:flex-row justify-between items-center">
         <div class="flex items-center mb-4 md:mb-0">
-            <img id="logo" src="images/logomionero.png" alt="Logo" class="h-16 mx-4 transition duration-300">
+            <p class="hover:text-lime-400 font-extrabold md:ml-4 md:order-1 md:mb-0 mb-2 text-2xl">&lt;I.M.DEV&gt;</p>
         </div>
         <a href="/bio" class="hover:text-lime-400 font-semibold md:ml-4 md:order-1 md:mb-0 mb-2">ABOUT</a>
         <div class="text-center md:order-2">
             <p class="font-semibold">RAVENNA</p>
         </div>
         <div class="flex space-x-4 mx-4 md:order-3">
-            <a href="https://www.instagram.com" class="text-gray-300 hover:text-lime-400">
+            <a href="https://www.instagram.com" class="hover:text-lime-400 text-3xl">
                 <i class="fa-solid fa-envelope"></i>
             </a>
-            <a href="https://www.linkedin.com/in/mattia-iaconawebdev" class="text-gray-300 hover:text-lime-400">
+            <a href="https://www.linkedin.com/in/mattia-iaconawebdev" class="hover:text-lime-400 text-3xl">
                 <i class="fab fa-linkedin"></i>
             </a>
-            <button id="theme-toggle" class="text-gray-300 hover:text-lime-400">
+            <button id="theme-toggle" class="hover:text-lime-400 text-3xl">
                 <i id="theme-icon" class="fa-solid fa-rotate"></i>
             </button>
         </div>
