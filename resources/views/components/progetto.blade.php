eac95c676189434965f5c3a570d2dd0171b17f88
Mattia Iacona <mattiaiacona87@icloud.com>
2024-10-19 01:33:47 +0200

    sistemata design vista progetto

--- resources/views/components/progetto.blade.php
+++ resources/views/components/progetto.blade.php
@@ -1,10 +1,14 @@
 <x-layout>
-    <section class="grid md:grid-cols-2 gap-4 m-12 mb-36" style="min-height: 900px">
+    <section class="grid md:grid-cols-2 justify-center items-center gap-4 m-12 mb-36" style="min-height: 900px">
         <!-- Prima colonna (Immagine del progetto) -->
-        <div class="flex justify-center items-center">
-            <img src="{{ asset($project['image']) }}" alt="Immagine progetto" class="p-3 bg-gray-100 rounded-lg shadow-2xl w-80 h-auto">
+        <div class="relative flex justify-center items-center">
+            <!-- Forma verde irregolare -->
+            <div class="absolute bg-gradient-to-r from-lime-500 via-lime-400 to-lime-300 w-80 h-80 md:w-96 md:h-96 rounded-full -z-10 transform -rotate-12"></div>
+            
+            <!-- Immagine del progetto -->
+            <img src="{{ asset($project['image']) }}" alt="Immagine progetto" class="p-3 w-64 md:w-80 h-auto">
         </div>
-
+    
         <!-- Seconda colonna (Dettagli del progetto) -->
         <div class="flex justify-center items-center text-center">
             <div>
@@ -15,4 +19,5 @@
             </div>
         </div>
     </section>
+    
 </x-layout>
