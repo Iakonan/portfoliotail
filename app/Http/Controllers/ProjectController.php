2ed81aa597f4a716409055e32d3a935ba0639ded
Mattia Iacona <mattiaiacona87@icloud.com>
2024-10-24 17:38:30 +0200

    sistemato design vista progetti, terminata vista livingwood e iniziata vista presto

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -10,22 +10,100 @@ class ProjectController extends Controller
     {
         $progetti = [
             'livingwoodfb' => [
-                'title' => 'LivingwoodFB',
-                'description' => "LivingWoodFB è un e-commerce dedicato alla vendita di fingerboard fatti a mano, progettato con un'interfaccia utente intuitiva e dinamiche standard per il commercio online. Il sito offre un catalogo di prodotti che include deck da fingerboard, magliette e accessori, e fornisce la possibilità di ordinare tavole personalizzate tramite un form dedicato. Tra le funzionalità principali, LivingWoodFB supporta la navigazione multilingue (inglese, spagnolo e italiano) per un pubblico internazionale. Il processo di check-out è ottimizzato per garantire un'esperienza di acquisto semplice e sicura, con gestione della spedizione e fatturazione automatica, inviata direttamente all'amministratore del sito. Il sito include anche una sezione &quot;About Us&quot;, che racconta la storia e la mission del brand, aiutando i clienti a connettersi con l'artigianalità dei prodotti.",
-                'image' => 'images/logotrasp.png',
-                'link' => '/livingwoodfb'
+                'title1' => 'Panoramica del sito e catalogo prodotti',
+                'title2' => 'Navigazione multilingue e accesso globale',
+                'title3' => 'Processo di check-out e gestione della spedizione',
+                'description1' => "LivingWoodFB è un e-commerce interamente dedicato alla vendita di fingerboard fatti a mano, 
+                                con un focus sull'artigianalità e la personalizzazione. Il sito è stato progettato per offrire 
+                                un'interfaccia utente intuitiva, dove gli appassionati di fingerboard possono esplorare un 
+                                catalogo di prodotti selezionati con cura. La gamma comprende deck da fingerboard realizzati 
+                                artigianalmente, magliette con design esclusivi e accessori correlati. L'obiettivo principale 
+                                è creare un'esperienza d'acquisto immersiva e su misura per un pubblico di nicchia che apprezza 
+                                la qualità dei prodotti fatti a mano, nonché la possibilità di ordinare tavole personalizzate. 
+                                Il form di richiesta di tavole personalizzate consente ai clienti di specificare dettagli unici, 
+                                offrendo un grado di personalizzazione elevato che rende ogni acquisto speciale.",
+                'description2' => "Una delle caratteristiche fondamentali di LivingWoodFB è la sua accessibilità a un pubblico 
+                                internazionale grazie alla navigazione multilingue. Il sito è disponibile in tre lingue principali: 
+                                inglese, spagnolo e italiano. Questo permette ai clienti di diverse regioni geografiche di interagire 
+                                facilmente con il brand, abbattendo le barriere linguistiche e rendendo l'esperienza di acquisto più 
+                                fluida e piacevole per chiunque. L'implementazione multilingue non solo amplia la portata del negozio 
+                                online, ma dimostra l'impegno di LivingWoodFB nel creare una piattaforma inclusiva, in cui ogni cliente 
+                                può sentirsi accolto, indipendentemente dalla propria lingua madre.",
+                'description3' => "Il processo di check-out su LivingWoodFB è stato ottimizzato per garantire un'esperienza d'acquisto 
+                                semplice, rapida e sicura. L'integrazione di un sistema di pagamento affidabile e sicuro permette ai 
+                                clienti di completare gli ordini senza complicazioni, mentre la gestione automatizzata della spedizione 
+                                assicura che i prodotti siano inviati tempestivamente. La fatturazione, generata automaticamente per 
+                                ogni ordine, viene inviata direttamente all'amministratore del sito per semplificare la gestione 
+                                contabile e garantire trasparenza e precisione. In aggiunta, la sezione &quot;About Us&quot; fornisce una 
+                                connessione emotiva con i clienti, raccontando la storia del brand e trasmettendo i valori di autenticità 
+                                e passione per il fingerboard.",
+                'image1' => 'images/logotrasp.png',
+                'image2' => 'images/decks.jpg',
+                'image3' => 'images/trick.jpg',
             ],
             'prestoit' => [
-                'title' => 'Presto.IT',
-                'description' => 'Descrizione di Presto.IT...',
-                'image' => 'images/logopresto.png',
-                'link' => '/prestoit'
+                'title1' => 'Panoramica del sito e catalogo prodotti',
+                'title2' => 'Navigazione multilingue e accesso globale',
+                'title3' => 'Processo di check-out e gestione della spedizione',
+                'description1' => "LivingWoodFB è un e-commerce interamente dedicato alla vendita di fingerboard fatti a mano, 
+                                con un focus sull'artigianalità e la personalizzazione. Il sito è stato progettato per offrire 
+                                un'interfaccia utente intuitiva, dove gli appassionati di fingerboard possono esplorare un 
+                                catalogo di prodotti selezionati con cura. La gamma comprende deck da fingerboard realizzati 
+                                artigianalmente, magliette con design esclusivi e accessori correlati. L'obiettivo principale 
+                                è creare un'esperienza d'acquisto immersiva e su misura per un pubblico di nicchia che apprezza 
+                                la qualità dei prodotti fatti a mano, nonché la possibilità di ordinare tavole personalizzate. 
+                                Il form di richiesta di tavole personalizzate consente ai clienti di specificare dettagli unici, 
+                                offrendo un grado di personalizzazione elevato che rende ogni acquisto speciale.",
+                'description2' => "Una delle caratteristiche fondamentali di LivingWoodFB è la sua accessibilità a un pubblico 
+                                internazionale grazie alla navigazione multilingue. Il sito è disponibile in tre lingue principali: 
+                                inglese, spagnolo e italiano. Questo permette ai clienti di diverse regioni geografiche di interagire 
+                                facilmente con il brand, abbattendo le barriere linguistiche e rendendo l'esperienza di acquisto più 
+                                fluida e piacevole per chiunque. L'implementazione multilingue non solo amplia la portata del negozio 
+                                online, ma dimostra l'impegno di LivingWoodFB nel creare una piattaforma inclusiva, in cui ogni cliente 
+                                può sentirsi accolto, indipendentemente dalla propria lingua madre.",
+                'description3' => "Il processo di check-out su LivingWoodFB è stato ottimizzato per garantire un'esperienza d'acquisto 
+                                semplice, rapida e sicura. L'integrazione di un sistema di pagamento affidabile e sicuro permette ai 
+                                clienti di completare gli ordini senza complicazioni, mentre la gestione automatizzata della spedizione 
+                                assicura che i prodotti siano inviati tempestivamente. La fatturazione, generata automaticamente per 
+                                ogni ordine, viene inviata direttamente all'amministratore del sito per semplificare la gestione 
+                                contabile e garantire trasparenza e precisione. In aggiunta, la sezione &quot;About Us&quot; fornisce una 
+                                connessione emotiva con i clienti, raccontando la storia del brand e trasmettendo i valori di autenticità 
+                                e passione per il fingerboard.",
+                'image1' => 'images/logopresto.png',
+                'image2' => 'images/prestohome.png',
+                'image3' => 'images/prestorev.png',
             ],
             'questboard' => [
-                'title' => 'Questboard',
-                'description' => 'Descrizione di Questboard...',
-                'image' => 'images/QuestBoard.png',
-                'link' => '/questboard'
+                'title1' => 'Panoramica del sito e catalogo prodotti',
+                'title2' => 'Navigazione multilingue e accesso globale',
+                'title3' => 'Processo di check-out e gestione della spedizione',
+                'description1' => "LivingWoodFB è un e-commerce interamente dedicato alla vendita di fingerboard fatti a mano, 
+                                con un focus sull'artigianalità e la personalizzazione. Il sito è stato progettato per offrire 
+                                un'interfaccia utente intuitiva, dove gli appassionati di fingerboard possono esplorare un 
+                                catalogo di prodotti selezionati con cura. La gamma comprende deck da fingerboard realizzati 
+                                artigianalmente, magliette con design esclusivi e accessori correlati. L'obiettivo principale 
+                                è creare un'esperienza d'acquisto immersiva e su misura per un pubblico di nicchia che apprezza 
+                                la qualità dei prodotti fatti a mano, nonché la possibilità di ordinare tavole personalizzate. 
+                                Il form di richiesta di tavole personalizzate consente ai clienti di specificare dettagli unici, 
+                                offrendo un grado di personalizzazione elevato che rende ogni acquisto speciale.",
+                'description2' => "Una delle caratteristiche fondamentali di LivingWoodFB è la sua accessibilità a un pubblico 
+                                internazionale grazie alla navigazione multilingue. Il sito è disponibile in tre lingue principali: 
+                                inglese, spagnolo e italiano. Questo permette ai clienti di diverse regioni geografiche di interagire 
+                                facilmente con il brand, abbattendo le barriere linguistiche e rendendo l'esperienza di acquisto più 
+                                fluida e piacevole per chiunque. L'implementazione multilingue non solo amplia la portata del negozio 
+                                online, ma dimostra l'impegno di LivingWoodFB nel creare una piattaforma inclusiva, in cui ogni cliente 
+                                può sentirsi accolto, indipendentemente dalla propria lingua madre.",
+                'description3' => "Il processo di check-out su LivingWoodFB è stato ottimizzato per garantire un'esperienza d'acquisto 
+                                semplice, rapida e sicura. L'integrazione di un sistema di pagamento affidabile e sicuro permette ai 
+                                clienti di completare gli ordini senza complicazioni, mentre la gestione automatizzata della spedizione 
+                                assicura che i prodotti siano inviati tempestivamente. La fatturazione, generata automaticamente per 
+                                ogni ordine, viene inviata direttamente all'amministratore del sito per semplificare la gestione 
+                                contabile e garantire trasparenza e precisione. In aggiunta, la sezione &quot;About Us&quot; fornisce una 
+                                connessione emotiva con i clienti, raccontando la storia del brand e trasmettendo i valori di autenticità 
+                                e passione per il fingerboard.",
+                'image1' => 'images/logotrasp.png',
+                'image2' => 'images/decks.jpg',
+                'image3' => 'images/trick.jpg',
             ],
         ];
 
--- resources/views/components/progetto.blade.php
+++ resources/views/components/progetto.blade.php
@@ -1,44 +1,58 @@
 <x-layout>
-    <section class="grid md:grid-cols-2 justify-center items-center gap-4 m-12 mb-9" style="min-height: 900px">
+    <section class="grid md:grid-cols-2 justify-center items-center gap-4 mt-12 mx-12 mb-12">
         <!-- Prima colonna (Immagine del progetto) -->
         <div class="relative flex justify-center items-center mb-10">
             <!-- Forma verde irregolare -->
             <div class="absolute bg-gradient-to-r from-lime-500 via-lime-400 to-lime-300 w-80 h-80 md:w-96 md:h-96 rounded-full -z-10 transform -rotate-12"></div>
             
             <!-- Immagine del progetto -->
-            <img src="{{ asset($project['image']) }}" alt="Immagine progetto" class="p-3 w-64 md:w-80 h-auto">
+            <img src="{{ asset($project['image1']) }}" alt="Immagine progetto" class="p-3 w-64 md:w-80 h-auto">
         </div>
     
         <!-- Seconda colonna (Dettagli del progetto) -->
-        <div class="flex justify-center items-center text-center">
+        <div class="flex justify-center items-center md:text-end p-5">
             <div>
-                <h1 class="text-5xl font-extrabold mb-8">{{ $project['title'] }}</h1>
-                <p class="text-lg font-semibold uppercase text-justify">
-                    {{ $project['description'] }}
+                <h1 class="md:text-5xl text-2xl uppercase font-extrabold mb-8">{{ $project['title1'] }}</h1>
+                <p class="text-lg font-semibold uppercase">
+                    {{ $project['description1'] }}
                 </p>
             </div>
         </div>
     </section>
-    <section class="grid md:grid-cols-2 justify-center items-center gap-4 m-12 mb-72" style="min-height: 900px">
+    <section class="grid md:grid-cols-2 justify-center items-center gap-4 mx-12 md:mt-36">
         
         <!-- Prima colonna (Dettagli del progetto) -->
-        <div class="flex justify-center items-center text-center">
+        <div class="flex justify-center items-center md:text-start p-5">
             <div>
-                <h1 class="text-5xl font-extrabold mb-8">{{ $project['title'] }}</h1>
-                <p class="text-lg font-semibold uppercase text-justify">
-                    {{ $project['description'] }}
+                <h1 class="md:text-5xl text-2xl uppercase font-extrabold mb-8">{{ $project['title2'] }}</h1>
+                <p class="text-lg font-semibold uppercase">
+                    {{ $project['description2'] }}
                 </p>
             </div>
         </div>
         <!-- Seconda colonna (Immagine del progetto) -->
-        <div class="relative flex justify-center items-center">
-            <!-- Forma verde irregolare -->
-            <div class="absolute bg-gradient-to-r from-lime-500 via-lime-400 to-lime-300 w-80 h-80 md:w-96 md:h-96 rounded-full -z-10 transform -rotate-12"></div>
-            
+        <div class="relative flex justify-center items-center mt-12">
             <!-- Immagine del progetto -->
-            <img src="{{ asset($project['image']) }}" alt="Immagine progetto" class="p-3 w-64 md:w-80 h-auto">
+            <img src="{{ asset($project['image2']) }}" alt="Immagine progetto" class="w-64 md:w-auto h-auto rounded-3xl border-4 border-lime-400">
         </div>
-    
     </section>
+    <section class="grid md:grid-cols-2 justify-center items-center gap-4 mx-12 md:mt-36 mt-12 mb-72">
+        
+        <!-- Prima colonna (Dettagli del progetto) -->
+        <div class="flex justify-center items-center md:text-start p-5">
+            <div>
+                <h1 class="md:text-5xl text-2xl uppercase font-extrabold mb-8">{{ $project['title3'] }}</h1>
+                <p class="text-lg font-semibold uppercase">
+                    {{ $project['description3'] }}
+                </p>
+            </div>
+        </div>
+        <!-- Seconda colonna (Immagine del progetto) -->
+        <div class="relative flex justify-center items-center mt-12">
+            <!-- Immagine del progetto -->
+            <img src="{{ asset($project['image3']) }}" alt="Immagine progetto" class="w-64 md:w-auto h-auto rounded-3xl border-4 border-lime-400">
+        </div>
+    </section>
+
     
 </x-layout>
